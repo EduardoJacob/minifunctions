1d1f039085c49ee31e09321509cd0419d01d83da
Eduardo Jacob <eduardo.jorge.jacob@hotmail.com>
2026-04-29 21:49:11 +0100

    support for emojis

--- R/tableprop.R
+++ R/tableprop.R
@@ -9,7 +9,13 @@
 #'
 tableprop = function(v) {
   tab = table(v)
-  print( cbind(Count = tab, Proportion = prop.table(tab)) )
+  df = data.frame(Count = tab, Proportion = prop.table(tab))
+  df$Proportion.v = NULL
+  names(df) = c(deparse(substitute(v)),"Count", "Proportion")
+  # colnames becomes disaligned when there are emojis
+  # print( as_tibble(df) )
+  # knitr::kable(df)
+  print(df)
 }
 
 
