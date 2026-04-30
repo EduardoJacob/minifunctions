ab96a6d0e3febd5c2cd4ad38598fd41cc9ad1474
Eduardo Jacob <eduardo.jorge.jacob@hotmail.com>
2026-05-01 00:57:14 +0100

    New functions added: serpapi and imagegrid

---
+++ R/imagegrid.R
@@ -0,0 +1,67 @@
+#' Display the images in a folder as a grid using the magick package
+#'
+#' @param folder Images folder
+#' @param ncol Optional grid number of columns
+#' @param nrow Optional grid number of rows
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' imagegrid("P:/DISKD/Youtube R Programming/LOGOS")
+#' }
+imagegrid = function(folder, ncol=0, nrow=0) {
+  # Check if folder exists
+  if ( !dir.exists(folder) ) {
+    message("folder not found ",folder)
+    return(NULL)
+  }
+
+  file_paths = vector()
+
+  # loop through the files in the folder
+  N = 0
+  for ( file in list.files(folder) ) {
+    extension = tolower( tools::file_ext(file) )
+
+    # if extension not in the list of image extensions, skip the file
+    if ( extension  %notin% c("jpg", "jpeg", "png", "bmp", "gif") ) next
+
+    file_path = file.path(folder, file)
+
+    if ( file.info(file_path)$isdir ) next
+
+    N = N + 1
+    file_paths = c(file_paths, file_path)
+
+  }
+
+  if ( N == 0 ) {
+    message("No image files found in ",folder)
+    return(NULL)
+  }
+
+  # Read the images using magick
+  images = lapply(file_paths, magick::image_read)
+
+  # if ncol or nrow is not specified, calculate them based on the number of images
+  if ( ncol == 0 || nrow == 0 ) {
+    ncol = ceiling(sqrt(N))
+    nrow = ceiling(N / ncol)
+  }
+
+  # Combine the images into a grid
+  grid_image = magick::image_montage(
+    do.call(c, images),
+    tile = paste(ncol, nrow, sep = "x"),
+    geometry = "320x180+10+10",
+    bg = "#505050"
+  )
+
+  # Display the grid image
+  print(grid_image)
+}
+
+
+
+
---
+++ R/serpapi.R
@@ -0,0 +1,53 @@
+#' Submit a prompt to SerpApi
+#'
+#' @param prompt Text prompt
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' serpapi("Qual a capital de Portugal?")
+#' }
+serpapi = function(prompt) {
+  message("Calling SerpApi: ", prompt )
+
+  SERPAPI_KEY = Sys.getenv("SERPAPI_KEY")
+
+  start_time = Sys.time()
+
+  answer = httr2::request("https://serpapi.com/search.json") |>
+    httr2::req_url_query(
+      engine = "google",
+      q = prompt,
+      api_key = SERPAPI_KEY
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  answer = answer[["organic_results"]]
+
+  # 2. Extract the specific fields from 'organic_results'
+  answer = answer |>
+    purrr::map_dfr(~list(
+      source  = .x$source,
+      title   = .x$title,
+      snippet = .x$snippet
+    ))
+
+  # html = markdown::mark_html(text = answer)
+  #
+  # temp_file = tempfile(fileext = ".html")
+  # writeLines(html, temp_file)
+  #
+  # rstudioapi::viewer(temp_file)
+
+  time_diff = difftime(Sys.time(), start_time, units = "secs")
+  message("Elapsed ", round(time_diff, 2), " seconds.")
+
+  # save html on current directory
+  # writeLines(html,"sepapi.html")
+
+  return(answer)
+
+}
+
